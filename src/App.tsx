fbe630b7fc9c105db06da167f73ed6c6bebf0d8c
gihanviduranga <gihanvidu123@gmai.com>
2025-05-16 01:14:52 +0530

    App file update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 
 import './App.css'
 
+import "tailwindcss";
+import HeroMain from "./components/HeroSection/HeroMain.tsx";
+import MainLayout from "./MainLayout.tsx";
+import AboutSection from "./components/AboutSection.tsx";
+import SkillsSection from "./components/SkillsSection.tsx";
+import ProjectsSection from "./components/ProjectsSection.tsx";
+import ContactSection from "./components/ContactSection.tsx";
+
 function App() {
 
   return (
-    <>
-
-    </>
+      <MainLayout>
+          <HeroMain />
+          <AboutSection />
+          <SkillsSection />
+          <ProjectsSection />
+          <ContactSection />
+      </MainLayout>
   )
 }
 
